5c6d35ba4aa91809dfb8a0d8f04a0b89cb025c0d
blnche <peltierblanche@gmail.com>
2024-03-20 17:29:09 +0100

    toggle password visibility done done

--- js/show-hide-password.js
+++ js/show-hide-password.js
@@ -6,8 +6,5 @@ export function passwordVisibilityToggle(){
     } else {
         passwordInput.type = "password";
     }
-}
 
-// check id
-// add input checkbox with onclick="passwordVisibilityToggle()" to show or hide 
-// do i need to call function in main too ?
+}
